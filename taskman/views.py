a88ff6fd505ce3fd0cf0684a51de94cd5bbc7812
makrandr1999 <makrand.r@somaiya.edu>
2019-03-06 10:25:21 +0530

    Modify views.py to handle registration

--- taskman/views.py
+++ taskman/views.py
@@ -8,4 +8,28 @@ from django.contrib import messages
 def homepage(request):
     return render(request = request,
                   template_name='taskman/home.html')
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request,f"New Account Created: {username}")
+            login(request, user)
+            messages.info(request,f"You are now logged in as {username}")
+
+            return redirect("taskman:homepage")
+
+        else:
+            for msg in form.error_messages:
+                messages.error(request, f"{msg}:{form.error_messages[msg]}")
+
+            return render(request = request,
+                          template_name = "taskman/register.html",
+                          context={"form":form})
+
+    form = UserCreationForm
+    return render(request = request,
+                  template_name = "taskman/register.html",
+                  context={"form":form})                  
 # Create your views here.
