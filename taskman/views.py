7e19fc93c082d6ecf08aed291037a1cf4eca3fa0
makrandr1999 <makrand.r@somaiya.edu>
2019-03-06 10:17:11 +0530

    Add register template

--- taskman/views.py
+++ taskman/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import logout, authenticate, login
+from django.contrib import messages
 
 
 # Create your views here.
