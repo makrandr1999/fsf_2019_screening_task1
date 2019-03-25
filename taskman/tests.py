391c3f3b098a4ef51b836f77c0fddce28782d626
makrandr1999 <makrand.r@somaiya.edu>
2019-03-25 16:56:51 +0530

    Add team and comment model test cases

--- taskman/tests.py
+++ taskman/tests.py
@@ -1,21 +1,22 @@
 from django.test import TestCase
 from .models import Task,Team,Comment
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your tests here.
 
 '''
-user2 = User(pk=2,username='zack', password='zack')
-user2.save()
+
 '''
 
 class TaskModelTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         # Set up non-modified objects used by all test methods
-        #user=User.objects.get(id=11)
         user1 = User(pk=1,username='tomisbest', password='tom123456789')
         user1.save()
+        user2 = User(pk=2,username='zack', password='zack')
+        user2.save()
         team = Team(pk=1,name='Team 1',creator=user1.username)
         team.save()
         team.members.set(User.objects.all())
@@ -26,26 +27,23 @@ class TaskModelTest(TestCase):
         task.save()
         
 
-    def test_data_test(self):
+    def test_tasks_data_check(self):
         task = Task.objects.get(pk=1)
         assignees=task.assignee.get(username='tomisbest')
-        self.assertEquals(assignees.username,'tomisbest')
+        self.assertEqual(assignees.username,'tomisbest')
         self.assertEqual(task.creator.username, 'tomisbest')
         self.assertEqual(task.title, 'test title')
         self.assertEqual(task.text, 'test title description')
-        self.assertEquals(task.status, 'Planned')
-        self.assertEquals(task.team.name, 'Team 1')
+        self.assertEqual(task.status, 'Planned')
+        self.assertEqual(task.team.name, 'Team 1')
+    
+
+    
+    def test_title_max_length(self):
+        task = Task.objects.get(pk=1)
+        max_length = task._meta.get_field('title').max_length
+        self.assertEquals(max_length, 200)
     '''
-    def test_date_of_death_label(self):
-        author=Author.objects.get(id=1)
-        field_label = author._meta.get_field('date_of_death').verbose_name
-        self.assertEquals(field_label, 'died')
-
-    def test_first_name_max_length(self):
-        author = Author.objects.get(id=1)
-        max_length = author._meta.get_field('first_name').max_length
-        self.assertEquals(max_length, 100)
-
     def test_object_name_is_last_name_comma_first_name(self):
         author = Author.objects.get(id=1)
         expected_object_name = f'{author.last_name}, {author.first_name}'
@@ -56,3 +54,58 @@ class TaskModelTest(TestCase):
         # This will also fail if the urlconf is not defined.
         self.assertEquals(author.get_absolute_url(), '/catalog/author/1')
     '''
+class TeamModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        user1 = User(pk=3,username='tomisworst', password='tom123456789')
+        user1.save()
+        user2 = User(pk=4,username='zackisbest', password='zack')
+        user2.save()
+        team = Team(pk=1,name='Team abc',creator=user1.username)
+        team.save()
+        team.members.set(User.objects.all())
+        team.save() 
+
+    def test_team_data_check(self):
+        team = Team.objects.get(pk=1)
+        self.assertEqual(team.name,'Team abc')
+        self.assertEqual(team.creator, 'tomisworst')
+        self.assertEqual(team.members.all()[0].username,'tomisworst')
+        self.assertEqual(team.members.all()[1].username,'zackisbest')
+
+    def test_name_max_length(self):
+        team = Team.objects.get(pk=1)
+        max_length = team._meta.get_field('name').max_length
+        self.assertEquals(max_length, 64)    
+
+class CommentModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        user1 = User(pk=1,username='tomisbest', password='tom123456789')
+        user1.save()
+        user2 = User(pk=2,username='zack', password='zack')
+        user2.save()
+        team = Team(pk=1,name='Team 1',creator=user1.username)
+        team.save()
+        team.members.set(User.objects.all())
+        team.save() 
+        task=Task(pk=1,creator=user1,title='test title',text='test title description',team=team,status='Planned')
+        task.save()
+        task.assignee.set(User.objects.filter(id=1))
+        task.save()
+        comment=Comment(pk=1,task=task,author=user1.username,text='Awesome work')
+        comment.save()
+
+    def test_comment_data_check(self):
+        comment = Comment.objects.get(pk=1)
+        self.assertEqual(comment.task.title,'test title')
+        self.assertEqual(comment.author, 'tomisbest')
+        self.assertEqual(comment.text,'Awesome work')
+
+    '''
+    def test_name_max_length(self):
+        team = Team.objects.get(pk=1)
+        max_length = team._meta.get_field('name').max_length
+        self.assertEquals(max_length, 64) 
+    '''      
+
